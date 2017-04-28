17950c93b781b59f09b482ac3fa382c3b5a555db
Vladyslav <dovhopol.vladyslav@fabware.com>
2017-04-28 20:37:01 +0300

    Write stub test to check that Travis CI runs it.

--- test/access/AccessControllerTest.java
+++ test/access/AccessControllerTest.java
@@ -1,21 +1,22 @@
 package access;
 
-import io.restassured.http.ContentType;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static io.restassured.RestAssured.get;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by Vladyslav Dovhopol on 4/27/17.
  * Test Greeting Controller.
  */
+@RunWith(SpringRunner.class)
 public class AccessControllerTest {
 
     @Test
-    public void GET_NoPayload_Correct() {
-        get("/greeting").
-        then().assertThat().
-                statusCode(200).and().
-                contentType(ContentType.HTML);
+    public void assertThatNullIsNull() {
+        assertThat(null, is(nullValue()));
     }
 }
