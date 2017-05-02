96aee4269637a7fef28f82211545041d957528e7
Vladyslav <dovhopol.vladyslav@fabware.com>
2017-05-02 17:38:03 +0300

    Configure Thymeleaf template resolver.

--- source/config/WebApplicationConfiguration.java
+++ source/config/WebApplicationConfiguration.java
@@ -1,16 +1,32 @@
 package config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafView;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.StandardTemplateModeHandlers;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Vladyslav Dovhopol
  */
 @Configuration
 @EnableWebMvc
-public class WebApplicationConfiguration extends WebMvcConfigurerAdapter {
+public class WebApplicationConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -19,17 +35,35 @@ public class WebApplicationConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("ui");
+        registry.addResourceHandler("resources");
     }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 
+        viewResolver.setTemplateEngine(createThymeleafTemplateEngine());
         viewResolver.setViewClass(ThymeleafView.class);
         viewResolver.setOrder(1);
-        viewResolver.setViewNames(new String[]{"*.html"});
+        viewResolver.setViewNames(new String[]{"*"});
 
         registry.viewResolver(viewResolver);
+    }
+
+    private SpringTemplateEngine createThymeleafTemplateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(createThymeleafTemplateResolver());
+        return engine;
+    }
+
+    private ITemplateResolver createThymeleafTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("resources/templates/");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resolver.setTemplateMode(StandardTemplateModeHandlers.HTML5.getTemplateModeName());
+        resolver.setName("Default Thymeleaf template resolver");
+        return resolver;
     }
 }
