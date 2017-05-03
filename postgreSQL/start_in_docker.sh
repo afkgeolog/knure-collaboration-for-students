602c4110008e4bbbd4ab1b9c1a0a1268ea87690f
Vladyslav <dovhopol.vladyslav@fabware.com>
2017-05-03 13:35:55 +0300

    Add environmental variables to run PostgreSQL in Docker.

--- postgreSQL/start_in_docker.sh
+++ postgreSQL/start_in_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #Docker must be running.
-#Script that kills running PostgreSQL depends on specified container name.
-docker run --name PostgreSql -d postgres:9.6.2
+#Script that kills running PostgreSQL depends on container name.
+docker run --name PostgreSql -d -p=5432:5432/tcp -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+        -e POSTGRES_USER="$POSTGRES_USER" -e POSTGRES_DB="$POSTGRES_DB" postgres:9.6.2
