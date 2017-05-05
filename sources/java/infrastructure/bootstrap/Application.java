6dd10ee784763f31716420130f7de74b8d2fe031
Vladyslav <dovhopol.vladyslav@fabware.com>
2017-05-05 14:56:27 +0300

    Configure Flyway.
    Contains test scripts to test on CI.

--- sources/java/infrastructure/bootstrap/Application.java
+++ sources/java/infrastructure/bootstrap/Application.java
@@ -3,9 +3,12 @@ package infrastructure.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"access", "infrastructure.config"})
+@PropertySources({@PropertySource(value = "flyway.properties", encoding = "UTF-8")})
 public class Application {
 
     public static void main(String[] args) {
