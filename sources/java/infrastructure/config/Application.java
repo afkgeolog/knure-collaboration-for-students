e35aa2abdf9d062cce4ff671571fba4a244373e6
Vladyslav <dovhopol.vladyslav@fabware.com>
2017-05-19 13:48:51 +0300

    Move Application class from package "bootstrap" to package "config".
    This class is a configuration.

--- sources/java/infrastructure/config/Application.java
+++ sources/java/infrastructure/config/Application.java
@@ -1,14 +1,18 @@
-package infrastructure.bootstrap;
+package infrastructure.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"access", "infrastructure.config"})
+@ComponentScan(basePackages = {"access", "infrastructure.config", "schedule"})
 @PropertySources({@PropertySource(value = "flyway.properties", encoding = "UTF-8")})
+@EnableAsync
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
