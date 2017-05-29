8dd30d51b95d09f1df80ee1aa4bf7c4fe7db9fad
Vladyslav <dovhopol.vladyslav@fabware.com>
2017-05-29 11:08:43 +0300

    Run docker in Travis CI.
    Needed for integration tests that use database.

--- postgreSQL/kill_in_docker.sh
+++ postgreSQL/kill_in_docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+#Be cautious when renaming or moving this script is may be used somewhere.
 docker kill PostgreSql
 docker rm PostgreSql
--- postgreSQL/start_in_docker.sh
+++ postgreSQL/start_in_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Docker must be running.
 #Script that kills running PostgreSQL depends on container name.
+#Be cautious when renaming or moving this script is may be used somewhere.
 docker run --name PostgreSql -d -p=5432:5432/tcp -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
         -e POSTGRES_USER="$POSTGRES_USER" -e POSTGRES_DB="$POSTGRES_DB" postgres:9.6.2
